2bcbac23d100553f6968fa4c2517fd8ee054534a
Ángel Millán <138896454+angelrmh@users.noreply.github.com>
2024-10-17 10:46:54 -0400

    Add files via upload

--- 01-Terraform/AWS/01-VPC+EC2/4-ec2.tf
+++ 01-Terraform/AWS/01-VPC+EC2/4-ec2.tf
@@ -1,11 +1,7 @@
 resource "aws_instance" "my-Terraform-ec2" {
-  count         = 1
-  ami           = "ami-04a81a99f5ec58529"
-  instance_type = "t2.micro"
-  # Aqui le paso un rol que voy a crear llamado "test_role"
-  # iam_instance_profile = "${aws_iam_instance_profile.poc_profile.name}"
-  # Aqui le paso un rol que ya esta creado
-  # iam_instance_profile = "demo" 
+  count                       = 1
+  ami                         = "ami-04a81a99f5ec58529"
+  instance_type               = "t2.micro"
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.ec2.id]
