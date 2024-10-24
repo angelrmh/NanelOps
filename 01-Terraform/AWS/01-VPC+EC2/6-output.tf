bf393cf3d191571bbac726d05e1c350d748f7ca6
Ángel Millán <138896454+angelrmh@users.noreply.github.com>
2024-10-24 13:41:22 -0400

    add file via upload for code update on 6-output.tf

--- 01-Terraform/AWS/01-VPC+EC2/6-output.tf
+++ 01-Terraform/AWS/01-VPC+EC2/6-output.tf
@@ -1,4 +1,4 @@
 # instance EC2 Public IP 
 output "my-Terraform-ec2-public-ip" {
-  value = aws_instance.my-Terraform-ec2.associate_public_ip_address
+  value = aws_instance.my-Terraform-ec2.public_ip
 }
